2c96250f00e915e5b8e7c8bc64baad36809add08
Mike <mikejon0829@gmail.com>
2017-08-26 12:37:17 -0700

    Added copy and delete files script

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
 const reload = browserSync.reload;
+const clean = require('gulp-clean');
 
 var SOURCEPATHS = {
-	sassSource : 'src/scss/*.scss'
+	sassSource : 'src/scss/*.scss',
+	htmlSource : 'src/*.html',
+	jsSource : 'src/js/*.js'
 }
 
 var APPPATH = {
@@ -13,15 +17,37 @@ var APPPATH = {
 	js : 'app/js'
 }
 
+gulp.task('copy', ['clean-html'], function() {
+	return gulp.src(SOURCEPATHS.htmlSource)
+			.pipe(gulp.dest(APPPATH.root));
+});
+
+
+gulp.task('scripts', ['clean-scripts'], function() {
+	return gulp.src(SOURCEPATHS.jsSource)
+			.pipe(gulp.dest(APPPATH.js));
+});
+
+gulp.task('clean-html', function() {
+	return gulp.src(APPPATH.root + '*.html', { read:false, force: true })
+			.pipe(clean());
+});
+
+gulp.task('clean-scripts', function() {
+	return gulp.src(APPPATH.js + '/*.js', { read:false, force: true })
+			.pipe(clean());
+});
+
+//compile sass
 gulp.task('sass', function() {
 	return gulp.src(SOURCEPATHS.sassSource)
-	.pipe(sass({
-		outputStyle : 'expanded'}).on('error', sass.logError))
+	.pipe(autoprefixer())
+	.pipe(sass({ outputStyle : 'expanded'}).on('error', sass.logError))
 	.pipe(gulp.dest(APPPATH.css));
 });
 
-gulp.task('serve', [ 'sass' ], function() {
-	
+gulp.task('serve', [ 'sass', 'copy', 'scripts' ], function() {
+	//initialize small server with files synchronized
 	browserSync.init([ APPPATH.css + '/*.css', APPPATH.root + '*.html',
 			APPPATH.js + '/*.js' ], {
 		server : {
@@ -29,7 +55,10 @@ gulp.task('serve', [ 'sass' ], function() {
 		}
 	});
 	
+	//run sass after change detected in file
 	gulp.watch(SOURCEPATHS.sassSource, ['sass']);
+	gulp.watch(SOURCEPATHS.htmlSource, ['copy']);
+	gulp.watch(SOURCEPATHS.jsSource, ['scripts']);
 	
 });
 
