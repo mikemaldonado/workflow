ef3f02b585c5a5237d8d803455fc831341bf7a0e
Mike <mikejon0829@gmail.com>
2017-08-25 20:06:51 -0700

    browser sync added

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,36 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
+
+var SOURCEPATHS = {
+	sassSource : 'src/scss/*.scss'
+}
+
+var APPPATH = {
+	root : 'app/',
+	css : 'app/css',
+	js : 'app/js'
+}
 
 gulp.task('sass', function() {
-	return gulp.src('src/scss/app.scss')
-		.pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
-		.pipe(gulp.dest('app/css'));
+	return gulp.src(SOURCEPATHS.sassSource)
+	.pipe(sass({
+		outputStyle : 'expanded'}).on('error', sass.logError))
+	.pipe(gulp.dest(APPPATH.css));
 });
 
-gulp.task('default', ['sass']);
+gulp.task('serve', [ 'sass' ], function() {
+	
+	browserSync.init([ APPPATH.css + '/*.css', APPPATH.root + '*.html',
+			APPPATH.js + '/*.js' ], {
+		server : {
+			baseDir : APPPATH.root
+		}
+	});
+	
+	gulp.watch(SOURCEPATHS.sassSource, ['sass']);
+	
+});
+
+gulp.task('default', [ 'serve' ]);
