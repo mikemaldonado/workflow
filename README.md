49d0d1e5d13dd42f5a0002bb09851624dc814184
Mike <mikejon0829@gmail.com>
2017-09-05 15:51:15 -0700

    Added readme file and file minification

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# workflow
+# My workflow with NPM, Sass, Gulp
 My web workflow with NPM, SASS, Gulp and more
+
+# Instructions
+1.-Clone this repo.
+2.-Make sure these are installed
+	- [node.js](http://nodejs.org/)
+	- [git](http://git-scm.com/)
+	- [gulp](http://gulpjs.com/)
+3.- Run `npm install ` to install the project dependencies
--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,29 @@ const browserSync = require('browser-sync').create();
 const autoprefixer = require('gulp-autoprefixer');
 const reload = browserSync.reload;
 const clean = require('gulp-clean');
+const browserify = require('gulp-browserify');
+const concat = require('gulp-concat');
+const merge = require('merge-stream');
+const newer = require('gulp-newer');
+const imagemin = require('gulp-imagemin');
+const minify = require('gulp-minify');
+const minifycss = require('gulp-cssmin');
+const rename = require('gulp-rename');
+const htmlmin = require('gulp-htmlmin');
 
 var SOURCEPATHS = {
 	sassSource : 'src/scss/*.scss',
 	htmlSource : 'src/*.html',
-	jsSource : 'src/js/*.js'
+	jsSource : 'src/js/*.js',
+	imgSource : 'src/img/**'
 }
 
 var APPPATH = {
 	root : 'app/',
 	css : 'app/css',
-	js : 'app/js'
+	js : 'app/js',
+	fonts : 'app/fonts',
+	img : 'app/img'
 }
 
 gulp.task('copy', ['clean-html'], function() {
@@ -22,11 +34,18 @@ gulp.task('copy', ['clean-html'], function() {
 			.pipe(gulp.dest(APPPATH.root));
 });
 
+gulp.task('moveFonts', function() {
+	gulp.src('./node_modules/bootstrap/fonts/*.{svg,eot,ttf,woff,woff2}')
+		.pipe(gulp.dest(APPPATH.fonts));
+});
 
 gulp.task('scripts', ['clean-scripts'], function() {
 	return gulp.src(SOURCEPATHS.jsSource)
+			.pipe(concat('main.js'))
+			.pipe(browserify())
 			.pipe(gulp.dest(APPPATH.js));
 });
+
 
 gulp.task('clean-html', function() {
 	return gulp.src(APPPATH.root + '*.html', { read:false, force: true })
@@ -38,15 +57,62 @@ gulp.task('clean-scripts', function() {
 			.pipe(clean());
 });
 
-//compile sass
-gulp.task('sass', function() {
-	return gulp.src(SOURCEPATHS.sassSource)
-	.pipe(autoprefixer())
-	.pipe(sass({ outputStyle : 'expanded'}).on('error', sass.logError))
-	.pipe(gulp.dest(APPPATH.css));
+//minify image
+gulp.task('images', function() {
+	return gulp.src(SOURCEPATHS.imgSource)
+		.pipe(newer(APPPATH.img))
+		.pipe(imagemin())
+		.pipe(gulp.dest(APPPATH.img));
 });
 
-gulp.task('serve', [ 'sass', 'copy', 'scripts' ], function() {
+//compile sass
+gulp.task('sass', function() {
+	
+	var bootstrapCSS = gulp.src('./node_modules/bootstrap/dist/css/bootstrap.css');
+	var sassFiles;
+	
+	sassFiles = gulp.src(SOURCEPATHS.sassSource)
+		.pipe(autoprefixer())
+		.pipe(sass({ outputStyle : 'expanded'}).on('error', sass.logError));
+	
+	return merge(sassFiles, bootstrapCSS)
+		.pipe(concat('app.css'))
+		.pipe(gulp.dest(APPPATH.css));
+});
+
+/**Production Tasks**/
+gulp.task('compressjs', ['clean-scripts'], function() {
+	return gulp.src(SOURCEPATHS.jsSource)
+			.pipe(concat('main.js'))
+			.pipe(browserify())
+			.pipe(minify())
+			.pipe(gulp.dest(APPPATH.js));
+});
+
+gulp.task('compresscss', function() {
+	
+	var bootstrapCSS = gulp.src('./node_modules/bootstrap/dist/css/bootstrap.css');
+	var sassFiles;
+	
+	sassFiles = gulp.src(SOURCEPATHS.sassSource)
+		.pipe(autoprefixer())
+		.pipe(sass({ outputStyle : 'expanded'}).on('error', sass.logError));
+	
+	return merge(sassFiles, bootstrapCSS)
+		.pipe(concat('app.css'))
+		.pipe(minifycss())
+		.pipe(rename({suffix : '.min'}))
+		.pipe(gulp.dest(APPPATH.css));
+});
+
+gulp.task('compresshtml', ['clean-html'], function() {
+	return gulp.src(SOURCEPATHS.htmlSource)
+			.pipe(htmlmin({collapseWhitespace: true}))
+			.pipe(gulp.dest(APPPATH.root));
+});
+/**End of Production**/
+
+gulp.task('serve', ['sass', 'copy', 'scripts', 'moveFonts', 'images'], function() {
 	//initialize small server with files synchronized
 	browserSync.init([ APPPATH.css + '/*.css', APPPATH.root + '*.html',
 			APPPATH.js + '/*.js' ], {
@@ -59,7 +125,9 @@ gulp.task('serve', [ 'sass', 'copy', 'scripts' ], function() {
 	gulp.watch(SOURCEPATHS.sassSource, ['sass']);
 	gulp.watch(SOURCEPATHS.htmlSource, ['copy']);
 	gulp.watch(SOURCEPATHS.jsSource, ['scripts']);
+	gulp.watch(SOURCEPATHS.imgSource, ['images']);
 	
 });
 
 gulp.task('default', [ 'serve' ]);
+gulp.task('production', ['compresscss', 'compressjs', 'compresshtml']);
